12812d1269d931b6dd06fc092e9235c8391fcc0c
tlu17 <tlu17@illinois.edu>
2022-03-21 15:20:13 -0700

    Initial Commit

---
+++ MyStockList/MyStockListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStockListApp.swift
+//  MyStockList
+//
+//  Created by Tiff Lu on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStockListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
